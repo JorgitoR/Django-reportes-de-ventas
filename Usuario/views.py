d88e24675e3ee553a632eaf6f021d9ef1f00f591
JorgitoR <jorgitouribe133@gmail.com>
2021-05-26 12:08:37 -0500

    Django reportes de ventas

--- Usuario/views.py
+++ Usuario/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DeleteView, UpdateView, DetailView
+from django.views.generic import ListView, DeleteView, UpdateView, DetailView, CreateView
 from django.http import JsonResponse
 
 from .models import Usuario
+from .forms import UsuarioForm
 
 class UsuarioListaView(ListView):
 	model = Usuario
@@ -30,4 +31,32 @@ class UsuarioListaView(ListView):
 		context['craer_url'] = reverse_lazy()
 		context['lista_url'] = reverse_lazy()
 		context['entidad'] = 'Usuarios'
+		return context
+
+class UsuarioCrearView(CreateView):
+	model = Usuario
+	form_class = UsuarioForm
+	template_name = 'usuario/crear.html'
+	success_url = reverse_lazy()
+
+	def post(self, request, *args, **kwargs):
+		data = {}
+		try:
+			action = request.POST['action']
+
+			if action == 'add':
+				form = self.get_form()
+				data = form.save()
+			else:
+				data['error']="No has ingresado alguna opcion"
+		except Exception as e:
+			data['error'] = str(e)
+		return JsonResponse(data)
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['titulo'] = 'Creacion de usuarios'
+		context['entidad'] ='Usuarios'
+		context['lista_url'] = self.success_url
+		context['action'] = 'add'
 		return context
