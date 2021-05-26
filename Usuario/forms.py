a457c4cf339b36ed6c0ab24a122ad8d4184f6a1d
JorgitoR <jorgitouribe133@gmail.com>
2021-05-26 11:08:57 -0500

    Django reportes de ventas

--- Usuario/forms.py
+++ Usuario/forms.py
@@ -1,3 +1,26 @@
 from django import forms
 from .Usuario import Usuario
 
+class UsuarioForm(forms.ModelForm):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['first_name'].widget.attrs['autofocus'] = True
+
+	class Meta:
+		model = Usuario
+		fields = [
+			'first_name',
+			'last_name',
+			'email',
+			'username',
+			'password',
+			'image',
+			'groups'
+
+		]
+
+		widget = {
+
+		
+		}
