a6978d833e071a11db0f17a5fb2ed696d4ac0107
JorgitoR <jorgitouribe133@gmail.com>
2021-05-26 12:03:47 -0500

    Django reportes de ventas

--- Usuario/views.py
+++ Usuario/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, UpdateView, DetailView
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Usuario
+
+class UsuarioListaView(ListView):
+	model = Usuario
+	template_name = 'usuario/lista.html'
+
+	def post(self, request, *args, **kwargs):
+		data = {}
+		try:
+			action = request.POST['action']
+			if action == 'searchdata':
+				data = []
+				for i in Usuario.objects.all():
+					data.append(i.toJSON())
+			else:
+				data['Error'] = 'Ha ocurrido un error'
+
+		except Exception as e:
+			data['error'] = str(e)
+		return JsonResponse(data, safe=False)
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['titulo'] = 'Listado de Usuarios'
+		context['craer_url'] = reverse_lazy()
+		context['lista_url'] = reverse_lazy()
+		context['entidad'] = 'Usuarios'
+		return context
