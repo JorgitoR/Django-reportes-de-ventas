69a700aa23459fa7acbc95be0f78764969fb147b
JorgitoR <jorgitouribe133@gmail.com>
2021-05-26 11:14:03 -0500

    Django reportes de ventas

--- Usuario/forms.py
+++ Usuario/forms.py
@@ -22,5 +22,54 @@ class UsuarioForm(forms.ModelForm):
 
 		widget = {
 
-		
+			'first_name': forms.TextInput(
+				attrs={
+					'placeholder':'Ingrese su nombre',
+				}
+			),
+			'last_name' forms.TextInput(
+				attrs={
+					'placeholder':'Ingrese su apellido',
+				}
+			),
+
+			'email': forms.TextInput(
+				attrs={
+					'placeholder':'Ingrese su Email'
+				}
+			),
+
+			'username': forms.TextInput(
+				attrs={
+					'placeholder':'Ingrese su usurname'
+				}
+			),
+
+			'password': forms.PasswordInput(render_value=True,
+
+				attrs={
+					'placeholder':'Ingrese su password'
+				}
+			),
+
+			'groups': forms.SelectMultiple(
+				attrs={
+					'class': 'form-control select2'
+					'style': ' width:100%',
+					'multiple': 'multiple'
+				}
+			)
+
 		}
+
+		exclude = [
+			'user_permissions',
+			'last_login',
+			'date_joined',
+			'is_superuser',
+			'is_active',
+			'is_staff'
+
+		]
+
+	def save(self, commit=True):
